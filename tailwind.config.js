729d10ecccab3ea40d82db1753a28662fa68475b
simple1017 <simple01281017@gmail.com>
2022-06-01 20:41:54 +0800

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [ "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      transitionProperty: {
+        'border-width': 'border-width',
+      }
+    },
+  },
+  plugins: [],
+}

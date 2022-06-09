f8c260cadb38c0b508cbd24a351251a713e23b25
simple1017 <simple01281017@gmail.com>
2022-06-09 10:12:18 +0800

    add the tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: [ "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '500px',
+      'sm':'640px',
+      'md':'768px',
+      'lg':'1024px'
+      },
     extend: {
       transitionProperty: {
         'border-width': 'border-width',
